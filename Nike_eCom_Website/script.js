e62d1b22903a439c0f340520a5021f148e138c04
Shaktisinh0044 <spparmar045@gmail.com>
2025-12-28 10:05:34 +0530

    Fix submodules and add projects as normal folders

---
+++ Nike_eCom_Website/script.js
@@ -0,0 +1,8 @@
+const hamburger = document.getElementById("hamburger");
+const menu = document.getElementById("menu");
+const bars = document.querySelector("fa-bars");
+
+hamburger.addEventListener("click", () => {
+  menu.classList.toggle("mobile-list-active");
+  bars.classList.toggle("fa-xmark");
+});
